b32e9af8ca1c9bb0ab8a239fb36829270d1c0318
s179059 <s179059@SEA-8D0AB3>
2017-10-10 17:22:57 -0700

    soooo many

--- MeethLib/vec2.cpp
+++ MeethLib/vec2.cpp
@@ -4,11 +4,35 @@
 #include <cfloat>
 
 
-vec2 operator+(const vec2 & lhs, const vec2 & rhs)
+vec2 operator+(const vec2 &lhs, const vec2 & rhs)
 {
 	
 	return{ lhs.x + lhs.x,lhs.y + lhs.y };
 }
+
+vec2 operator-(const vec2 &lhs, const vec2 & rhs)
+{
+	return{ lhs.x - lhs.x,lhs.y - lhs.y };
+}
+
+vec2 operator*(const vec2 &lhs, const vec2 & rhs)
+{
+
+	lhs.x * rhs.x;
+	lhs.y * rhs.y;
+	return lhs;
+}
+
+vec2 operator/(const vec2 &lhs, int rhs)
+{
+	vec2 value;
+	value.x = lhs.x / rhs;
+	value.y = lhs.y / rhs;
+	return value;
+}
+
+
+
 
 vec2 & operator+=(vec2 & lhs, const vec2 & rhs)
 {
@@ -16,6 +40,30 @@ vec2 & operator+=(vec2 & lhs, const vec2 & rhs)
 	lhs.y += rhs.y;
 	return lhs;
 }
+
+vec2 & operator-=(vec2 & lhs, const vec2 & rhs)
+{
+	lhs.x -= rhs.x;
+	lhs.y -= rhs.y;
+	return lhs;
+}
+
+vec2 & operator*=(vec2 & lhs, const vec2 & rhs)
+{
+	lhs.x *= rhs.x;
+	lhs.y *= rhs.y;
+	return lhs;
+}
+
+vec2 & operator/=(vec2 & lhs, const vec2 & rhs)
+{
+	lhs.x /= rhs.x;
+	lhs.y /= rhs.y;
+	return lhs;
+}
+
+
+
 
 bool operator==(const vec2 & lhs, const vec2 & rhs)
 {
@@ -26,12 +74,41 @@ bool operator==(const vec2 & lhs, const vec2 & rhs)
 	}
 }
 
-vec2 operator*(const vec2 & lhs, const vec2 & rhs)
+bool operator!=(const vec2 & lhs, const vec2 & rhs)
 {
-
-	lhs.x * rhs.x;
-	lhs.y * rhs.y;
-	return lhs;
+	if (abs(lhs.x - rhs.x) < FLT_EPSILON &&
+		abs(lhs.y - rhs.y) < FLT_EPSILON)
+	{
+		return false;
+	}
 }
 
 
+
+float vec2::operator[](int index) const
+{
+	return v[index];
+	
+}
+
+float & vec2::operator[](int index)
+{
+	return v[index];
+}
+
+float magnitood(vec2 a, vec2 b)
+{
+	float numa = (abs(a.x - b.x));
+	float numb = (abs(a.y - b.y));
+	numa *= numa;
+	numb *= numb;
+	float numc = numa + numb;
+	numc = sqrt(numc);
+	return numc;
+	
+}
+
+float normie(vec2 &a, vec2 &b,vec2 &c)
+{
+	c = c / magnitood(a, b);
+}
--- MeethLib/vec2.h
+++ MeethLib/vec2.h
@@ -7,16 +7,37 @@ union vec2
 	//acess by indiv component
 	struct { float x, y; };
 
+	float operator[](int index) const;
+	float &operator[](int index);
 };
 
 // arithmetic operators
 vec2 operator+(const vec2 &lhs, const vec2 &rhs);
+vec2 operator-(const vec2 &lhs, const vec2 &rhs);
+vec2 operator*(const vec2 &lhs, const vec2 &rhs);
+vec2 operator/(const vec2 &lhs, const vec2 &rhs);
 
 // compound assignment operators
 vec2 &operator+=(vec2 &lhs, const vec2 &rhs);
+vec2 &operator-=(vec2 &lhs, const vec2 &rhs);
+vec2 &operator*=(vec2 &lhs, const vec2 &rhs);
+vec2 &operator/=(vec2 &lhs, const vec2 &rhs);
 
 // logical operators
 bool operator==(const vec2 &lhs, const vec2 &rhs);
+bool operator!=(const vec2 &lhs, const vec2 &rhs);
 
 //multiply and 
-bool operator* (const vec2 &lhs, const vec2 &rhs);
+vec2 operator* (const vec2 &lhs, const vec2 &rhs);
+
+//divide 
+vec2 operator/ (const vec2 &lhs, const vec2 &rhs);
+
+//magnanimous tood part
+//Magnitude
+float magnitood(vec2 a);
+
+float normie(vec2 a);
+
+
+
